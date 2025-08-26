af22a2ef029cf5bce6553b8a7f23da9d53eb4cc6
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-08-26 19:38:00 +0330

    Day16: Setting up Listing images with FileManager Designing our interface

--- UIKit-test1/ViewController.swift
+++ UIKit-test1/ViewController.swift
@@ -7,11 +7,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UITableViewController {
+    var pic = [String]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        
+        let items = try! fm.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl"){
+                pic.append(item)
+            }
+        }
+        
+        print(pic)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pic.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "pic", for: indexPath)
+        cell.textLabel?.text = pic[indexPath.row]
+        return cell
     }
 
 
