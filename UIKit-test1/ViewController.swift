6e026e341b48bdd919412f0809d3716eb52c2d03
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-08-26 18:22:41 +0330

    Initial Commit

---
+++ UIKit-test1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIKit-test1
+//
+//  Created by Sothesom on 04/06/1404.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
