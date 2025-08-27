9adc39b3a1e112d8d1e7836b702fc78a348a8124
Sothesom <151612168+sotheso@users.noreply.github.com>
2025-08-27 12:41:41 +0330

    Day17: Building a detail screen Loading images with UIImage Final tweaks: hidesBarsOnTap, safe area margins

---
+++ UIKit-test1/DetaliViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetaliViewController.swift
+//  UIKit-test1
+//
+//  Created by Sothesom on 05/06/1404.
+//
+
+import UIKit
+
+class DetaliViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
